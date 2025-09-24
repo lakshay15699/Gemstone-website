21e73e88695b06b6d2ced6dc9c54d9dbd941c83a
lakshay15699 <blakshay66@gmail.com>
2025-09-24 20:38:36 +0530

    Updated nav bar and hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black opacity-50"></div>
 
       <div className="fixed z-10 flex flex-col h-full text-left text-white px-4">
-        <div className="text-3s font-bold mb-2 mt-20 text-left">Welcome to</div>
+        <div className="text-2xl font-bold mb-2 mt-20 text-left">Welcome to</div>
         <h1 className="text-8xl font-bold mb-4 text-left">Sayar Jewellers</h1>
        <div className="w-1/2">
         <p className="text-xl mb-6 mr-auto ">
@@ -19,9 +19,12 @@ const Hero = () => {
         </p>
         </div>
         <div>
-        <button className="inline-block bg-white-600 hover:bg-red-700 text-white px-6 py-3 rounded-lg border-2 border-white hover:border-transparent transition duration-300">
-          Explore Collections
-        </button>
+        <button className="inline-flex items-center bg-white-600 hover:bg-red-700 text-white px-6 py-3 rounded-lg border-2 border-white hover:border-transparent transition duration-300">
+  Explore Collections
+  <svg className="ml-2 w-5 h-5" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+  </svg>
+</button>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className=" px-6 py-3 flex items-center fixed top 0 left-0 w-full bg-white text-white bg-opacity-70 backdrop-blur-md z-50">
-       <div text-2xl font-bold> <img src="/images/sayar-white-logo.svg" alt="Logo" className="h-10 w-auto " /> </div>
-      
+    <nav className=" px-6 py-3 flex items-center fixed top 0 left-0 w-full bg-white text-white bg-opacity-0 backdrop-blur-md z-40">
+       <div text-2xl font-bold> <img src="/images/sayar-white-logo.svg" alt="Logo" className="h-13 w-auto pl-7" /> </div>
 
-        <ul className="flex list-none justify-around w-full max-w-md">
-            <li ><a href="#" className="p-8">COLLECTIONS</a></li>
-            <li ><a href="#" className="p-8">ABOUT</a></li>
-            <li ><a href="#" className="p-8">TESTIMONIALS</a></li>
-            <li ><a href="#" className="p-8">CONTACT</a></li>
-            <li> <a href="#" className="p-8">BLOG</a></li>    
-          <li> <a href="tel:9841068852" className="p-4">9841068852 </a></li>
+      <div className="flex-grow flex justify-center">
+
+        <ul className="flex list-none justify-between w-full max-w-2xl text-black font-semibold">
+            <li ><a href="#" className="p-6">COLLECTIONS</a></li>
+            <li ><a href="#" className="p-6">ABOUT</a></li>
+            <li ><a href="#" className="p-6">TESTIMONIALS</a></li>
+            <li ><a href="#" className="p-6">CONTACT</a></li>
+            <li> <a href="#" className="p-6">BLOG</a></li>    
+           
         </ul>
+        </div>
+        <div className="flex pl-20"><a href="tel:9841068852" className="p-4">9841068852 </a></div>
     </nav>
 
     
